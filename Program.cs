bcdf4f7a79430787d8ac7c7c55c4a604db97b796
Deepthi NP <65824317+Deepthi-NP-25@users.noreply.github.com>
2021-06-24 17:27:44 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace BodyMassIndex
+
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int weight, height, BodyMassIndex = 0;
+            Console.WriteLine("Enter the weight : ");
+            weight = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the height : ");
+            height = Convert.ToInt32(Console.ReadLine());
+
+            BodyMassIndex = weight / height;
+
+            Console.WriteLine("Results");
+            Console.WriteLine("BodyMassIndex {0}", BodyMassIndex);
+
+            if (BodyMassIndex < 18.5)
+            {
+                Console.WriteLine("Underweight");
+            }
+            else if (BodyMassIndex >= 25)
+            {
+                Console.WriteLine("Overweight");
+            }
+            else
+            {
+                Console.WriteLine("Healthy");
+            }
+
+
+
+        }
+    }
+}
