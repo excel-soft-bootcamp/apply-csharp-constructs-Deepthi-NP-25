b814e00eea524239ad8541f01b82d7179249d7b0
Deepthi NP <65824317+Deepthi-NP-25@users.noreply.github.com>
2021-06-24 20:58:29 +0530

    Add files via upload

---
+++ Height.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BodyMassIndex
+{
+    class Height
+    {
+        public int _value, height = 0;
+
+        Height(int height)
+        {
+            this._value = height;
+            
+        }
+
+    }
+}
+
