b61139329f93822866957d84df540cb43c2dd8ac
Deepthi NP <65824317+Deepthi-NP-25@users.noreply.github.com>
2021-06-25 11:54:15 +0530

    Add files via upload

---
+++ BodyMassIndex/BodyMassIndex.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Health
+{
+    class BodyMassIndex
+    {
+        public int weight, height;
+      
+
+        public int GetWeight()
+        {
+            return weight;
+        }
+        public int GetHeight()
+        {
+            return height;
+        }
+        public BodyMassIndex(int BodyMassIndex)
+        {
+            BodyMassIndex = weight / height;
+            
+        }
+
+        public BodyMassIndex()
+        {
+        BodyMassIndex bmi = new BodyMassIndex();
+        }
+    }
+    }
+
+        
+
+
+
+
+
+
+
+
+
