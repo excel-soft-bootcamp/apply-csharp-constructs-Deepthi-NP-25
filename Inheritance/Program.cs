667c4e15f2d173c3b3205e193cc33fa19b942812
Deepthi NP <65824317+Deepthi-NP-25@users.noreply.github.com>
2021-06-26 13:57:15 +0530

    Add files via upload

---
+++ Inheritance/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class Program
+    {
+        public static void main(String [] args)
+        {
+            Student studentbase = new Student("A01", "VTU", "Tom");
+            Teacher teacherbase = new Teacher("A01", "VTU", "Jerry");
+            Staff staffbase = new Staff("A01", "VTU", "Oswald");
+            Guest guestbase = new Guest("A01", "VTU", "Henry");
+
+            
+        }
+    }
+}
---
+++ Inheritance/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    public class Student : College
+    {
+        string _studentname;
+        public Student(string collegecode, string collegename, string studentname) :
+            base(collegecode, collegename)
+        {
+
+            this._studentname = studentname;
+        }
+    }
+}
+        
+
+
+
+
+
+
+
+
+
+
+
