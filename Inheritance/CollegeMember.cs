580ff053cd65fab5fecc332985ce6b07aea751a0
Deepthi NP <65824317+Deepthi-NP-25@users.noreply.github.com>
2021-06-26 23:39:39 +0530

    Add files via upload

---
+++ Inheritance/CollegeMember.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    public class CollegeMember
+    {
+        private string name, address;
+        /*public CollegeMember(string collegecode) 
+        {
+            this.collegecode = collegecode;
+        }
+
+        public CollegeMember(string collegecode, string collegename) : this(collegecode)
+        {
+
+            this.collegename = collegename;
+
+        }*/
+        public CollegeMember(string name) 
+        {
+            this.name = name;
+        }
+        public CollegeMember(string name, string address)
+            : this(name)
+        {
+            this.address = address;
+        }
+    }
+}
+
---
+++ Inheritance/Guest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    public class Guest : CollegeMember
+    {
+        private string persontovisit;
+        public Guest(string name, string address, string persontovisit) :
+            base(name, address)
+        {
+            this.persontovisit = persontovisit;
+        }
+    }
+}
+ 
+
+
+
+
+
