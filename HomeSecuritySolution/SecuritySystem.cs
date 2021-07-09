7299103123ae3e8a0fa6181e114ed14dca111b34
Deepthi NP <65824317+Deepthi-NP-25@users.noreply.github.com>
2021-07-09 21:46:21 +0530

    Add files via upload

---
+++ HomeSecuritySolution/SecuritySystem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeSecuritySolution
+{
+    public class SecuritySystem
+    {
+        public void Notify(string position)
+        {
+            this.SMS();
+        }
+        public void SMS()
+        {
+            Console.WriteLine("Position");
+        }
+
+    }
+}
