c0818d62b4a79b79d218f0d4d57d25dc319d7b7d
Deepthi NP <65824317+Deepthi-NP-25@users.noreply.github.com>
2021-07-10 20:37:08 +0530

    Add files via upload

---
+++ HomeSecuritySolution/Owner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeSecuritySolution
+{
+   public class Owner
+   {
+        public void HouseOwner(string position, string time)
+        {
+            Console.WriteLine(position, time);
+        }
+        
+
+
+   }
+}
---
+++ HomeSecuritySolution/SecuritySystem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeSecuritySolution
+{
+    public class SecuritySystem
+    {
+        public void Notify(string position)
+        {
+            Console.WriteLine("Door is" + position + "at" + DateTime.Now.ToString());
+        }
+        
+        
+    }
+}
