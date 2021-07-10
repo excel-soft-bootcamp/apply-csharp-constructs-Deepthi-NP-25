3e55ba69773ea9969698525e29fbb8d2c53dfb6d
Deepthi NP <65824317+Deepthi-NP-25@users.noreply.github.com>
2021-07-10 09:57:43 +0530

    Add files via upload

---
+++ HomeSecuritySolution/DoorSensor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeSecuritySolution
+{
+    public delegate void DoorPositionHandler(string position);
+    public class DoorSensor
+    {
+        string position;
+        public event DoorPositionHandler OnDoorPositionChanged;
+        public void Open()
+        {
+            position = "open";
+            OnDoorPositionChanged.Invoke(this.position);
+
+        }
+
+        public void Close()
+        {
+            position = "close";
+            OnDoorPositionChanged.Invoke(this.position);
+        }
+
+
+
+    }
+}
+
+
---
+++ HomeSecuritySolution/SecuritySystem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeSecuritySolution
+{
+    public class SecuritySystem
+    {
+        public void Notify(string position)
+        {
+            Console.WriteLine("Door is" + position + "at" + DateTime.Now.ToString());
+        }
+    }
+}
