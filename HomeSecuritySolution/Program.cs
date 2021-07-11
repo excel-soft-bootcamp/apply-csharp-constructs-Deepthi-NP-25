f23e984a00364035e03559e759c41a663d007844
Deepthi NP <65824317+Deepthi-NP-25@users.noreply.github.com>
2021-07-11 08:25:19 +0530

    Add files via upload

---
+++ HomeSecuritySolution/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeSecuritySolution
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            SecuritySystem _securitySystem = new SecuritySystem();
+            DoorPositionHandler doorPositionChangeHandler = new DoorPositionHandler(_securitySystem.Receive);
+
+
+            DoorSensor doorSensor = new DoorSensor();
+            doorSensor.OnDoorPositionChanged += doorPositionChangeHandler;
+            doorSensor.Open();
+            doorSensor.Close();
+
+
+        }
+    }
+}
