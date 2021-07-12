b3303f05219ab7a5383a9d915e7777def3b58243
Deepthi NP <65824317+Deepthi-NP-25@users.noreply.github.com>
2021-07-12 22:56:42 +0530

    Add files via upload

---
+++ HomeSecuritySolution/SecuritySystem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeSecuritySolution
+{
+    public class SecuritySystem
+    {
+        public void Receive(string message)
+        {
+            Console.WriteLine(message);
+            
+        }
+        
+        
+    }
+}
